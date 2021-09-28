8b31f70bbc46532e2617886ab7f664e9bc6fbb5d
LucasXXI <lucasleal2001@gmail.com>
2021-09-28 13:03:06 -0300

    Feat: Added documentation packages and methods in index.js, also started the initial documentation with swagger in the api.yaml file.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import Koa from "koa";
 import router from "./routes.js";
-import sequelize from './database/index.js'
+import sequelize from "./database/index.js";
+import cors from "@koa/cors";
+import swagger from "swagger2";
+import swagger2koa from "swagger2-koa";
+const { ui } = swagger2koa;
 
-sequelize.sync().then(() => console.log('[System] The DB is running...'));
+sequelize.sync().then(() => console.log("[System] The DB is running..."));
+
+const swaggerDocument = swagger.loadDocumentSync("src/api.yaml");
 
 const app = new Koa();
 
 app
-    .use(router.routes())
-    .use(router.allowedMethods());
+  .use(ui(swaggerDocument, "/documentation"))
+  .use(cors())
+  .use(router.routes())
+  .use(router.allowedMethods());
 
 const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT);
